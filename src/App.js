a20763b1e61776a2f977227268433aa1c9735c83
Andronikus <tiagoafroque@gmail.com>
2020-01-21 00:00:09 +0000

    added routing to navigation-item component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const ShopHats = () => (
+  <div>
+    <h1>Shop Hats</h1>
+  </div>
+)
+
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={HomePage}/>
+      <Route path='/shop/hats' component={ShopHats}/>
     </Switch>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'; 
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match}) => {
 
-    console.log(title, imageUrl);
+    console.log(history, match);
     return (
-    <div  className={`menu-item ${size}`}>
+    <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}>
 
         </div>
@@ -18,4 +19,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
     )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
