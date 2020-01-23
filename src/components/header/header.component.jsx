27b8d655f634bbd9432b71783d377101c8afbd58
Andronikus <tiagoafroque@gmail.com>
2020-01-23 13:01:38 +0000

    add Login link to header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,8 +11,9 @@ const Header = () => (
             <Logo className='logo' />
         </Link>
         <div className='options'>
-            <Link to='/shop' className='option'> SHOP</Link>
-            <Link to='/contact' className='option'> CONTACT</Link>
+            <Link to='/shop' className='option'>SHOP</Link>
+            <Link to='/contact' className='option'>CONTACT</Link>
+            <Link to='/signin' className='option'>SIGN IN</Link>
         </div>
     </div>
 );
