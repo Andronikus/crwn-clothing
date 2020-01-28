17ebaa9d19cc2aecf707de92fcca7ca059e5e5a7
Andronikus <tiagoafroque@gmail.com>
2020-01-28 22:07:32 +0000

    add cart dropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,6 +8,7 @@ import { ReactComponent as Logo} from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
 
 const Header = ({currentUser}) => (
@@ -25,6 +26,7 @@ const Header = ({currentUser}) => (
             }
             <CartIcon />
         </div>
+        <CartDropDown />
     </div>
 );
 
