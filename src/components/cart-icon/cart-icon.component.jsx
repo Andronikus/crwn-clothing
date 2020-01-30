b8e66ea361d1eea992b65e1d790e7a49ffdeed57
Andronikus <tiagoafroque@gmail.com>
2020-01-30 22:45:36 +0000

    count all items selected and show them in cart icon counter

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 
 import { connect } from 'react-redux';
-import {toogleHiddenValue} from '../../redux/cart/cart.actions';
+import { toogleHiddenValue } from '../../redux/cart/cart.actions';
 
-import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
 
-const CartIcon = ({toogleHiddenCart}) => (
-    <div className='cart-item-container' onClick= {toogleHiddenCart} >
-        <ShoppingIcon className='shopping-icon'/>
-        <span className='shopping-counter'>0</span>
+const CartIcon = ({ toogleHiddenCart, numberOfItems }) => (
+    <div className='cart-item-container' onClick={toogleHiddenCart} >
+        <ShoppingIcon className='shopping-icon' />
+        <span className='shopping-counter'>{numberOfItems}</span>
     </div>
 );
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    numberOfItems: cartItems.reduce((acc, item) => acc + item.quantity, 0)
+})
 
 const mapDispatchToProps = dispatch => ({
     toogleHiddenCart: () => dispatch(toogleHiddenValue())
 })
 
-
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
