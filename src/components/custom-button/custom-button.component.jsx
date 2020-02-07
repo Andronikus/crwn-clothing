540220136e96e8faa4cdaa26604194cbbe9cceb8
Andronikus <tiagoafroque@gmail.com>
2020-02-07 14:07:07 +0000

    styled custom button component with styled-components api

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import './custom-button.styles.scss';
+import { CustomButtonContainer } from './custom-button.styles';
 
-const CustomButton = ({ children, isGoogleSignIn, invertStyle, ...otherProps }) => (
-    <button className={`${invertStyle ? 'invert-colors' : ''} ${isGoogleSignIn ? 'google-button' : ''} custom-button`} {...otherProps}>{children}</button>
+const CustomButton = ({ children, ...props }) => (
+    <CustomButtonContainer {...props}> {children} </CustomButtonContainer>
 );
 
 export default CustomButton;
---
+++ src/components/custom-button/custom-button.styles.jsx
@@ -0,0 +1,61 @@
+import styles, { css } from 'styled-components';
+
+
+const styleButton = css`
+    background-color: black;
+    color: white;
+    border: none;
+`
+
+const styleGoogleButton = css`
+    background-color: #4285F4;
+    color: white;
+    border: none;
+
+    &:hover {
+    background-color: #357ac8;
+    color: white;
+    border: none;
+`
+const styleInvertedButton = css`
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+
+    &:hover {
+    background-color: black;
+    color: white;
+    border: none;
+`
+
+const styleButtonFromProps = (props) => {
+    if (props.isGoogleSignIn) {
+        return styleGoogleButton;
+    }
+
+    return props.invertStyle ? styleInvertedButton : styleButton;
+}
+
+
+export const CustomButtonContainer = styles.button`
+    min-width: 165px;
+    width: auto;
+    height: 50px;
+    letter-spacing: 0.5px;
+    line-height: 50px;
+    padding: 0 35px 0 35px;
+    font-size: 15px;
+    text-transform: uppercase;
+    font-family: 'Open Sans Condensed';
+    font-weight: bolder;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+  
+    &:hover {
+      background-color: white;
+      color: black;
+      border: 1px solid black;
+    }
+    ${styleButtonFromProps}
+`
