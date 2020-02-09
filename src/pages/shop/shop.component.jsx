97480bb2325b7cd3bac6f4f1d68370086e6f43fd
Andronikus <tiagoafroque@gmail.com>
2020-02-09 14:28:23 +0000

    added withSpinner component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,11 +4,24 @@ import { connect } from 'react-redux';
 
 import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 import CollectionPage from '../../pages/collection/collection.component';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
 
 import { firestore, createShopItemsFromCollectionsSnapshoot } from '../../firebase/firebase.utils';
 import { updateShop } from '../../redux/shop/shop.actions';
 
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+
 class ShopPage extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            isLoading: true,
+        }
+    }
 
     unsubscribeFromSnapshot = null;
 
@@ -17,6 +30,7 @@ class ShopPage extends React.Component {
         const collectionsRef = firestore.collection('collections');
         this.unsubscribeFromSnapshot = collectionsRef.onSnapshot(async onSnapshot => {
             updateShop(createShopItemsFromCollectionsSnapshoot(onSnapshot));
+            this.setState({ isLoading: false });
         })
 
     }
@@ -32,8 +46,8 @@ class ShopPage extends React.Component {
         const { match } = this.props;
         return (
             <div className="shop-page">
-                <Route exact path={`${match.path}`} component={CollectionOverview} />
-                <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
+                <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={this.state.isLoading} {...props} />} />
+                <Route path={`${match.path}/:categoryId`} render={(props) => <CollectionPageWithSpinner isLoading={this.state.isLoading} {...props} />} />
             </div>
         );
     }
