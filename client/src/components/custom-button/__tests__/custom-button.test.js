218230350b5cb32fb5118fb479be5d1f4a739eb0
Andronikus <tiagoafroque@gmail.com>
2020-04-26 23:13:50 +0100

    add circleci configuration

--- client/src/components/custom-button/__tests__/custom-button.test.js
+++ client/src/components/custom-button/__tests__/custom-button.test.js
@@ -5,7 +5,6 @@ import TestRenderer from 'react-test-renderer';
 
 import CustomButton from '../custom-button.component';
 
-
 it('render properly the button component', () => {
     const buttonComponent = TestRenderer.create(<CustomButton>Sign In</CustomButton>).toJSON();
     expect(buttonComponent).toMatchSnapshot();
