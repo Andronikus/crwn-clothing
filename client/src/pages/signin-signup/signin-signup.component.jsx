6643cf575c525122fcfa84192f47ef1e35ed0d6a
Andronikus <tiagoafroque@gmail.com>
2020-06-11 14:09:34 +0100

    fix remove event handler on signin/signup

--- client/src/pages/signin-signup/signin-signup.component.jsx
+++ client/src/pages/signin-signup/signin-signup.component.jsx
@@ -1,56 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import SignIn from '../../components/sign-in/sign-in.component';
-import SignUp from '../../components/sign-up/sign-up.component';
-import TextAsLink from '../../components/textAsLink/textAsLink.component';
+import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
+import TextAsLink from "../../components/textAsLink/textAsLink.component";
 
-import './signin-signup.styles.scss'
+import "./signin-signup.styles.scss";
 
 const SignInSignUp = () => {
-    const [smallDevice, setSmallDevice] = useState(false);
-    const [showSignin, setshowSignin] = useState(true);
+  const [smallDevice, setSmallDevice] = useState(false);
+  const [showSignin, setshowSignin] = useState(true);
 
-    useEffect(() => {
-        const isSmall = isSmallDevice(window.innerWidth, 800);
-        setSmallDevice(isSmall);
-        window.addEventListener('resize', resizeHandler);
+  useEffect(() => {
+    const isSmall = isSmallDevice(window.innerWidth, 800);
+    setSmallDevice(isSmall);
+    window.addEventListener("resize", resizeHandler);
 
-        return () => {
-            window.removeEventListener('resize');
-        }
-    }, []);
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, []);
 
-    function isSmallDevice(currentWidth, referenceWidth) {
-        return currentWidth <= referenceWidth;
+  function isSmallDevice(currentWidth, referenceWidth) {
+    return currentWidth <= referenceWidth;
+  }
+
+  function resizeHandler() {
+    setSmallDevice(isSmallDevice(window.innerWidth, 800));
+  }
+
+  const onClickHandler = () => {
+    setshowSignin(!showSignin);
+  };
+
+  let htmlToShow = (
+    <>
+      <SignIn />
+      <SignUp />
+    </>
+  );
+
+  if (smallDevice) {
+    if (showSignin) {
+      htmlToShow = (
+        <>
+          <SignIn />
+          <TextAsLink
+            clickHandler={onClickHandler}
+            text="Dont have an account yet?"
+          />
+        </>
+      );
+    } else {
+      htmlToShow = (
+        <>
+          <SignUp />
+          <TextAsLink clickHandler={onClickHandler} text="Take me to login!" />
+        </>
+      );
     }
+  }
 
-    function resizeHandler() {
-        setSmallDevice(isSmallDevice(window.innerWidth, 800));
-    }
-
-
-    const onClickHandler = () => {
-        setshowSignin(!showSignin);
-    }
-
-    let htmlToShow = (<><SignIn /><SignUp /></>);
-
-    if (smallDevice) {
-        if (showSignin) {
-            htmlToShow = (<><SignIn /><TextAsLink clickHandler={onClickHandler} text="Dont have an account yet?" /></>);
-        } else {
-            htmlToShow = (<><SignUp /><TextAsLink clickHandler={onClickHandler} text="Take me to login!" /></>);
-        }
-    }
-
-    return (
-        <div className='signIn-signUp'>
-            {
-                htmlToShow
-            }
-        </div>
-    );
-
+  return <div className="signIn-signUp">{htmlToShow}</div>;
 };
 
 export default SignInSignUp;
