237192fb6094f7f2bad03d0cfb54dacd6d163418
Andronikus <tiagoafroque@gmail.com>
2020-06-13 23:05:38 +0100

    add navigation to titles of shop overview sections

--- client/src/components/collection-preview/collection-preview.component.jsx
+++ client/src/components/collection-preview/collection-preview.component.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import CollectionItem from '../collection-item/collection-item.component';
+import CollectionItem from "../collection-item/collection-item.component";
 
-import './collection-preview.styles.scss';
+import "./collection-preview.styles.scss";
 
-
-const CollectionPreview = ({title, items}) => (
-    <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <div className='preview'>
-            {
-                items.filter( (item, idx) => idx < 4).map( item => (<CollectionItem key={item.id} item={item}/> ))
-            }
-        </div>
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <Link className="title" to={`/shop/${title.toLowerCase()}`}>
+      {title.toUpperCase()}
+    </Link>
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
+  </div>
 );
 
 export default CollectionPreview;
